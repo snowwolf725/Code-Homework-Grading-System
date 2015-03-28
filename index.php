a54c893a2791eab0b980b2321f5a42a7589d3dfa
SnowWolf725 <snowwolf725@gmail.com>
2015-03-28 15:07:03 +0800

    add bootstrap js

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <? session_start(); ?>
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link href="css/bootstrap.min.css" rel="stylesheet"
+
+    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0″>
+	
+    <script src="js/bootstrap.min.js"></script>
+	
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <title>ASGS</title>
 </head>
 
