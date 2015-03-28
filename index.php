7fc446dec994c916eaaff42fdba56c54a6815939
SnowWolf725 <snowwolf725@gmail.com>
2015-03-28 15:29:49 +0800

    replace index page

--- index.php
+++ index.php
@@ -1,37 +1,5 @@
-<? session_start(); ?>
-<html>
-<head>
-    <link href="css/bootstrap.min.css" rel="stylesheet"
-
-    <link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1.0″>
-	
-    <script src="js/bootstrap.min.js"></script>
-	
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-<title>ASGS</title>
-</head>
-
-<body leftmargin="0" topmargin="0" alink="#ffffcc" bgcolor="#000000" link="#ffffcc" marginheight="0" marginwidth="0" text="#ffffff" vlink="#ffffcc">
-<table border="0" cellpadding="0" cellspacing="0" width="800">
-  <!-- include head file -->
-  <tr><td colspan="2"><?php include 'head.html'; ?></td></tr>
-
-  <tr>
-    <!-- include list file -->
-    <td valign="top" width="212"><?php include 'list.php'; ?></td>
-
-    <td valign="top" width="588">
-		<font face="Verdana, Arial, Helvetica, sans-serif" size="3">
-		<? echo $_GET['msg']; ?>
-		</font>
-    </td>
-  </tr>
-
-  <!-- include foot file -->
-  <tr><td colspan="2"><?php include 'foot.html'; ?></td></tr>
-</table>
-</body>
-
-</html>
+<?
+	session_start();
+    require "../config.php";
+    $tpl->display('index.html');
+?>
